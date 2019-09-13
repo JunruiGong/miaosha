81a6c1a747a77950df4ad80a0bb4e1317d3c86f4
Junrui Gong <jgong8@hawk.iit.edu>
2019-09-12 19:45:40 -0500

    Mybatis-generator

--- src/main/java/miaosha/App.java
+++ src/main/java/miaosha/App.java
@@ -1,13 +1,39 @@
 package miaosha;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
+@RestController
+@MapperScan("com.miaoshaproject.dao")
+public class App {
+
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @RequestMapping
+    public String home() {
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null) {
+            return "this user is not exist!";
+        }
+        return "hello world!";
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 }
